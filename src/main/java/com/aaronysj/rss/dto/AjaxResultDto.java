d6ce344848796c6387168f9efcf5b4b8e4dac7b5
aaronysj <aaronysj@qq.com>
2021-01-13 09:30:18 +0800

    v1.0.3 -> 改错了

--- src/main/java/com/aaronysj/rss/dto/AjaxResultDto.java
+++ src/main/java/com/aaronysj/rss/dto/AjaxResultDto.java
@@ -2,15 +2,18 @@ package com.aaronysj.rss.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AjaxResultDto {
 
     private int code;
     private Object data;
+    private String version;
 
     public static AjaxResultDto ok(){
-        return new AjaxResultDto(200, null);
+        return new AjaxResultDto(200, null, null);
     }
 }
